1394e481a4340d49742ddda0dfb5a985de97a470
Blukezz <86196314+Blukezz@users.noreply.github.com>
2023-07-05 13:39:08 -0400

    Update ZenithAttackRifle.lua

--- src/scripts/ZenithAttackRifle.lua
+++ src/scripts/ZenithAttackRifle.lua
@@ -444,34 +444,26 @@ end
 Knife2.Transparency = 1
 KnifeH.Transparency = 0
 
-local Bullet = Global.RealChar:FindFirstChild("Bullet")
-if Bullet then
+local Bullet = Global.FlingPart
+if Bullet and not Global.PartDisconnected then
+	Global.PartDisconnected = true
 	if Bullet:FindFirstChild("AntiRotate") then
 		Bullet:FindFirstChild("AntiRotate"):Destroy()
 	end
-	Global.PartDisconnected = true
+	Global.CustomNotFlinging = true
 	local Mouse = game:GetService("Players").LocalPlayer:GetMouse()
 	local RootTo = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart
-	local Pos = Instance.new("BodyPosition")
-	Pos.MaxForce = Vector3.new(1,1,1)*math.huge
-	Pos.P = 25000
-	Pos.D = 125
-	Pos.Name = "Movement"
-	Pos.Position = Bullet.Position
-	Pos.Parent = Bullet
-	local Flinger = Instance.new("BodyAngularVelocity")
-	Flinger.MaxTorque = Vector3.new(1,1,1)*math.huge
-	Flinger.P = math.huge
-	Flinger.AngularVelocity = Vector3.new(5000,5000,5000)
-	Flinger.Name = "Flinger"
-	Flinger.Parent = Bullet
-	table.insert(Events, game:GetService("RunService").PostSimulation:Connect(function()
-		if shoote then
-			if Mouse.Target ~= nil then
-				Pos.Position = Mouse.Hit.Position
-			end
-		else
-			Pos.Position = roottohandle.Position
+	local Rotation = CFrame.Angles(math.random(-360,360),math.random(-360,360),math.random(-360,360))
+	table.insert(Global.TableOfEvents, game:GetService("RunService").Heartbeat:Connect(function()
+		Rotation = CFrame.Angles(math.random(-360, 360), math.random(-360, 360), math.random(-360, 360))
+		if Bullet and shoote and Mouse.Target ~= nil then
+			Global.Flinging = true
+			Bullet.RotVelocity = Vector3.new(0, 7500, 0)
+			Bullet.CFrame = Mouse.Hit * Rotation
+		elseif Bullet and not shoote then
+			Global.Flinging = true
+			Bullet.RotVelocity = Vector3.new(0, 7500, 0)
+			Bullet.CFrame = roottohandle.CFrame * Rotation
 		end
 	end))
 end
